a38de380abea12a2159a3faad8438df4b1289a52
KEA <lena_konovalova@rambler.ru>
2022-10-03 21:53:56 +0300

    добавлено решение

---
+++ задача/решение/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] array = new string[] {"1234", "1567", "-2", "cat", "hello"};
+string[] array2 = new string[array.Length];
+void FindNum(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+        
+    }
+    Console.Write("]");
+    Console.WriteLine("");
+}
+FindNum (array, array2);
+PrintArray(array);
+Console.WriteLine("Массив до изменения");
+PrintArray(array2);
+Console.WriteLine("Массив после изменения");
